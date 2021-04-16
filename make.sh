a09a8c9449bd4d9d78cc1d4d3517bdac6ca77ba6
Your Name <you@example.com>
2021-04-16 17:25:53 +0100

    Adjusted for new CPLD protocol and little cleanup

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-gcc fb2ami.c -o fb2ami -O3 -march=armv7 -I/opt/vc/include -L/opt/vc/lib -lbcm_host
-taskset 0x8 sudo ./bitbang
+gcc fb2ami.c ps_protocol.c -o fb2ami -O3 -march=armv7 -I/opt/vc/include -L/opt/vc/lib -lbcm_host
+taskset 0x8 sudo ./fb2ami
